a19d46ce4a909592f5e7cd3b24096374ca4b7498
Misha <mvorozhtsov@bk.ru>
2020-11-26 17:25:56 +0300

    add request

--- app/src/main/java/com/example/gaiety/MainActivity.kt
+++ app/src/main/java/com/example/gaiety/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.gaiety
 
+import android.app.job.JobInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.print.PrinterInfo
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toolbar
@@ -13,6 +15,8 @@ import com.example.gaiety.fragments.meFragment
 import com.example.gaiety.fragments.settingsFragment
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,6 +38,7 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+        fetchJson();
     }
 
     private fun makeCurrentFragment(fragment: Fragment) =
@@ -41,4 +46,28 @@ class MainActivity : AppCompatActivity() {
             replace(R.id.fl_wrapper, fragment)
             commit()
         }
+
+    
+    private fun fetchJson() {
+        val url = "https://api.timepad.ru/v1/events.json?limit=20&skip=0&cities=Москва,Санкт-Петербург&fields=location&sort=+starts_at"
+        val token = "4df6676a1f216b99b184c75fe65bb971b140473c"
+
+        val request = Request.Builder()
+            .url(url)
+            .header("Authorization", "Bearer " + token)
+            .build()
+
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object: Callback{
+            override fun onResponse(call: Call, response: Response) {
+                val body = response?.body?.string()
+                println(body)
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                println("Bad request")
+            }
+        })
+
+    }
 }
