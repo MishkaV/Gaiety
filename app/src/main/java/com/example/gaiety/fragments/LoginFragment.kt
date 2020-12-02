b73842f138abdc0959354d4669b7130020c1a1eb
Misha <mvorozhtsov@bk.ru>
2020-12-02 17:52:55 +0300

    fix merge

--- app/src/main/java/com/example/gaiety/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/gaiety/fragments/LoginFragment.kt
@@ -6,8 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.gaiety.R
-import com.google.android.material.textfield.TextInputLayout
-
 
 class LoginFragment : Fragment() {
 
